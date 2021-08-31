e8578ff67732461e8150c8dfc9565560dcf611a1
Rishit Dagli <rishit.dagli@gmail.com>
2021-08-31 08:43:04 +0530

    Run some function on top of LayerNormalization

--- fast_transformer/fast_transformer.py
+++ fast_transformer/fast_transformer.py
@@ -4,8 +4,9 @@ import tensorflow as tf
 class PreNorm(tf.keras.layers.Layer):
     def __init__(self, dim, fn):
         super(PreNorm, self).__init__()
+        self.fn = fn
         self.norm = tf.keras.layers.LayerNormalization(axis=-1)
 
     def call(self, x, **kwargs):
         x = self.norm(x)
-        return x
+        return self.fn(x)
